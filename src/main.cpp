bef05ba3d82553f0519248f44fec6c4076689e88
Akinyemi Adetunji <aadet001@hammer.cs.ucr.edu>
2015-04-19 19:56:04 -0700

    username and hostname added to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 using namespace std; 
 
@@ -16,12 +17,16 @@ int main(int argc, char **argv){
 	pid = fork();
 	if(pid == 0){
 		//child
-		execvp("ls", argv);
+		execvp(argv[1], argv);
 		exit(0);
 	}else {
+		//parent
 		wait(0);
 		cout << "hello world\n";
-
+		size_t buff;
+		char hostname[128];
+		gethostname(hostname, sizeof(hostname));
+		cout << getlogin() << "@" << hostname << ":" << endl;
 	}
 	
 
