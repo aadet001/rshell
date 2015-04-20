f4b3fa80f38e7884ef84931d807e11ac0df0c5b4
Akinyemi Adetunji <aadet001@hammer.cs.ucr.edu>
2015-04-19 17:47:21 -0700

    updated main.cpp. testing execvp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,19 @@ int main(int argc, char **argv){
 	for(int i = 0; i < argc; i++){
 		cout << i << ": " <<  argv[i] << endl;
 	}
+	
+	pid_t pid; 
+	pid = fork();
+	if(pid == 0){
+		//child
+		execvp("ls", argv);
+		exit(0);
+	}else {
+		wait(0);
+		cout << "hello world\n";
 
+	}
+	
 
 
 	return 0; 
